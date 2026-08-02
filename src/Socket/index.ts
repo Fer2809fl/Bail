2905f37ae45a848da3821409462935868a79ad27
𝕱𝖊𝖗𝖓𝖆𝖓𝖉𝖔 <179016253+Fer2809fl@users.noreply.github.com>
2026-08-02 17:43:21 -0600

    prueva de 3rror

--- src/Socket/index.ts
+++ src/Socket/index.ts
@@ -1,12 +1,25 @@
 import { DEFAULT_CONNECTION_CONFIG } from '../Defaults'
 import type { UserFacingSocketConfig } from '../Types'
+import { installConsoleFilter } from '../Utils/console-filter'
+import { printBanner } from '../Utils/banner'
 import { makeCommunitiesSocket } from './communities'
 
-// export the last socket layer
+
 const makeWASocket = (config: UserFacingSocketConfig) => {
+	installConsoleFilter()
+	printBanner()
+
 	const newConfig = {
 		...DEFAULT_CONNECTION_CONFIG,
 		...config
+	}
+
+	if (config.version && config.syncWaWebVersion === undefined) {
+		newConfig.syncWaWebVersion = false
+	}
+
+	if (!newConfig.version) {
+		newConfig.version = DEFAULT_CONNECTION_CONFIG.version
 	}
 
 	return makeCommunitiesSocket(newConfig)
