d0bc0f3a894c9aa4f903efb76e315bc07b574f08
𝕱𝖊𝖗𝖓𝖆𝖓𝖉𝖔 <179016253+Fer2809fl@users.noreply.github.com>
2026-08-02 17:43:04 -0600

    prueva de 3rror

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import makeWASocket from './Socket/index'
 
-// Banner de marca al cargar la librería
-// eslint-disable-next-line no-console
-console.log('\x1b[97m%s\x1b[0m\x1b[36m%s\x1b[0m', 'Baileys ', 'By ASTA-BOTS\n')
-
 export * from '../WAProto/index.js'
 export * from './Utils/index'
 export * from './Store/index'
