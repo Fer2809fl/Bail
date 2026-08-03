ebb7e6b729808c6deb2a5a7809ce6ee32232c4bb
𝕱𝖊𝖗𝖓𝖆𝖓𝖉𝖔 <179016253+Fer2809fl@users.noreply.github.com>
2026-08-02 20:33:44 -0600

    solucionado

--- lib/Utils/use-single-file-auth-state.d.ts
+++
@@ -1,6 +0,0 @@
-import type { AuthenticationState } from '../Types/index.js';
-export declare const useSingleFileAuthState: (fileName: string) => Promise<{
-    state: AuthenticationState;
-    saveCreds: () => void;
-}>;
-//# sourceMappingURL=use-single-file-auth-state.d.ts.map
