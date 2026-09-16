90b3d28c0b99d9a86b30ebe76651235498200044
𝕱𝖊𝖗𝖓𝖆𝖓𝖉𝖔 <179016253+Fer2809fl@users.noreply.github.com>
2026-09-16 11:44:16 -0600

    7.0.5

---
+++ lib/Utils/banner.d.ts
@@ -0,0 +1,8 @@
+export declare const BAILEYS_LOGO: string[];
+
+export declare function printBanner(options?: {
+    stream?: NodeJS.WritableStream;
+    frameMs?: number;
+    once?: boolean;
+    animate?: boolean;
+}): Promise<void>;
